c35a3776bf0dd6b02387458a684aba0030d4740d
piotr04j <darricck04@gmail.com>
2019-12-26 14:40:00 +0100

    added registration handle

---
+++ blog/src/Controller/RegistrationController.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use App\Form\UserType;
+use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    public function singUp
+    (
+        Request $request,
+        UserPasswordEncoderInterface $passwordEncoder,
+        EntityManagerInterface $entityManager,
+        Session $session
+    )
+    {
+        $form = $this->createForm(UserType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = new User($form->get('username')->getData(), $form->get('email')->getData());
+            $password = $passwordEncoder->encodePassword($user, $form->get('password')->getData());
+            $user->setPassword($password);
+            try {
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $session->getFlashBag()->add('success', sprintf('Account %s has been created!', $user->getUsername()));
+                return $this->redirectToRoute('homepage');
+            } catch (UniqueConstraintViolationException $exception) {
+                $session->getFlashBag()->add('danger', 'Email and username has to be unique');
+            }
+        }
+        return $this->render('authentication/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
